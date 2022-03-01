914791dffe774dbfa8e48ff39c53211f92c44a09
Ennia-Fahd <92646945+Ennia-Fahd@users.noreply.github.com>
2022-03-02 00:51:53 +0100

    Create compteEpargne.h

---
+++ compteEpargne.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "compte.h"
+#include "client.h"
+#include "mad.h"
+#include <iostream>
+using namespace std;
+
+class compteEpargne :
+    public compte
+{
+private:
+    float ti;
+public:
+    compteEpargne(client cl, mad s,float ti);
+    mad calcul_interet() const override;
+    bool retirer_argent(mad somme) override;
+    ~compteEpargne();
+};
